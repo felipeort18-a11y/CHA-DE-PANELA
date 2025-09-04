7d1abe539a1d2e37d1f5e02f4b1f1471142e797f
Felipe & Daiane <felipeort.18@gmail.com>
2025-09-04 13:00:49 -0300

    Delete src directory

--- src/GiftSite.jsx
+++
@@ -1,2 +0,0 @@
-import React, { useEffect, useMemo, useState } from "react";
-// (código completo que foi gerado no canvas, omitido aqui para simplificação)
--- src/main.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { GiftSite } from './src/GiftSite'
-import './index.css'
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><GiftSite /></React.StrictMode>,)
