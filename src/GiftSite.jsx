82d39cc4b9c5ba65ad4c2262486d141046c4f77c
felipeort18-a11y <felipeort.18@gmail.com>
2025-09-04 10:10:43 -0300

    Update GiftSite.jsx

--- src/GiftSite.jsx
+++ src/GiftSite.jsx
@@ -1,2 +1,2 @@
-import React, { useEffect, useMemo, useState } from "react";
+import { GiftSite } from './src/GiftSite';
 // (código completo que foi gerado no canvas, omitido aqui para simplificação)
