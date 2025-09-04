e2ce06beb8cf912bea0cd06bd4510a7dce89fc0e
felipeort18-a11y <felipeort.18@gmail.com>
2025-09-04 10:22:24 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import GiftSite from './GiftSite'
+import { GiftSite } from './src/GiftSite'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
