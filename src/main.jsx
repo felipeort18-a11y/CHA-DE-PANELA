8ab3b4ddd114d73bc540a47c211c9bfeb91c26d0
Felipe & Daiane <felipeort.18@gmail.com>
2025-09-04 10:30:17 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { GiftSite } from './src/GiftSite'
 import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <GiftSite />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><GiftSite /></React.StrictMode>,)
